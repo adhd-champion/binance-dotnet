57bf6b4caf499fe05424a796c3e255c808e24846
Greg <Greg@GPC2>
2017-11-05 20:24:33 -0700

    Added ignored classes

---
+++ binance-dotnet/objects/api_responses/Response_UserDataStream.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binance_dotnet.objects
+{
+    public class Response_UserDataStream : APIResponse
+    {
+        public string listenKey { get; set; }
+    }
+}
